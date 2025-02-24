f385f728ddfc910944e286e09e4e5ec7a4871255
Egor Sezen <sezen.egor@mail.ru>
2025-02-24 11:52:34 +0300

    Did new model and some changes.

--- bboard/views.py
+++ bboard/views.py
@@ -4,6 +4,6 @@ from .models import Bb
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
+    bbs = Bb.objects.all()
     context = {'bbs': bbs, 'title': 'Объявления'}
     return render(request, 'bboard/index.html', context)
