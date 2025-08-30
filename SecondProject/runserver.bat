8f3e791df81d7ff3057a72614f70d098eb0ba832
Sezen Egor <sezen.egor@mail.ru>
2025-08-30 14:41:09 +0300

    fix: use mkcert
    work without conflicts with ssl-certificate in Chrome

--- SecondProject/runserver.bat
+++ SecondProject/runserver.bat
@@ -4,10 +4,10 @@ call venv\Scripts\activate
 
 set HOST=127.0.0.1
 set PORT=8000
-
-set CERT_FILE=cert.crt
+set CERT_FILE=localhost+2.pem
+SET CERT_KEY=localhost+2-key.pem
 
 start "" https://%HOST%:%PORT%/
-python manage.py runserver_plus %HOST%:%PORT% --cert-file %CERT_FILE%
+python manage.py runserver_plus %HOST%:%PORT% --cert-file %CERT_FILE% --key-file %CERT_KEY%
 
 pause
