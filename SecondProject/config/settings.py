7fb7ee5996603511e37d2116685ee7fbbfc13dfe
sezen <sezen.egor@mail.ru>
2026-02-23 13:24:46 +0300

    chore: add django-redis DefaultClient option to CACHES config

--- SecondProject/config/settings.py
+++ SecondProject/config/settings.py
@@ -190,5 +190,8 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://localhost:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
     }
 }
