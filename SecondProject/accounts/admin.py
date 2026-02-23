9c8f4d11345d2192a51ffeb4d40d186f114a2290
sezen <sezen.egor@mail.ru>
2026-02-23 21:28:16 +0300

    refactor: replace register with ModelAdmin class and add full_name display field

--- SecondProject/accounts/admin.py
+++ SecondProject/accounts/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import AdvUser
 
-admin.site.register(AdvUser)
+
+@admin.register(AdvUser)
+class AdvUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "is_activated", "full_name")
+
+    @admin.display(title="Полное имя", ordering="user__last_name")
+    def full_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
