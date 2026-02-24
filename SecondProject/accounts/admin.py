e2df24492d06ca646edc2f154704dd1fb32551fd
sezen <sezen.egor@mail.ru>
2026-02-24 10:38:45 +0300

    fix: replace title with description in admin.display and add username method

--- SecondProject/accounts/admin.py
+++ SecondProject/accounts/admin.py
@@ -7,6 +7,9 @@ from .models import AdvUser
 class AdvUserAdmin(admin.ModelAdmin):
     list_display = ("username", "email", "is_activated", "full_name")
 
-    @admin.display(title="Полное имя", ordering="user__last_name")
+    @admin.display(description="Полное имя", ordering="user__last_name")
     def full_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
+
+    def username(self, obj):
+        return obj.user.username
