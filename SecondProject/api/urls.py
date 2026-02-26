dfac2055a1ac88ebf511cd1d512f25d82a7d4ffd
sezen <sezen.egor@mail.ru>
2026-02-27 01:58:06 +0300

    fix: rename rubric_detail URL to rubrics/<pk> for consistent REST naming

--- SecondProject/api/urls.py
+++ SecondProject/api/urls.py
@@ -3,6 +3,6 @@ from .views import api_rubrics, api_rubric_detail
 
 app_name = "api"
 urlpatterns = [
-    path("rubric_detail/<int:pk>/", api_rubric_detail, name="rubric_detail"),
+    path("rubrics/<int:pk>/", api_rubric_detail, name="rubric_detail"),
     path("rubrics/", api_rubrics, name="api_rubrics"),
 ]
