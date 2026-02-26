6544d523d5201a18b20b9ae5e46dc5e632a47dd0
sezen <sezen.egor@mail.ru>
2026-02-26 20:52:49 +0300

    feat: add rubric detail loading and form submission with list refresh

--- SecondProject/app/static/app/js/rubrics.js
+++ SecondProject/app/static/app/js/rubrics.js
@@ -10,10 +10,14 @@ listLoader.addEventListener('readystatechange', () => {
             let s = '<ul>', d;
             for (let i = 0; i < data.length; i++) {
                 d = data[i];
-                s += '<li>' + d.name + '</li>';
+                s += '<li>' + d.name + ' <a href="' + domain + '/api/rubric_detail/' + d.id + '/" class="detail">Вывести</a></li>';
             }
             s += '</ul>';
             list.innerHTML = s;
+            let links = list.querySelectorAll('ul li a.detail');
+            links.forEach((link) =>
+            {
+                link.addEventListener('click', rubricLoad);});
         } else
             window.alert(listLoader.statusText);
     }
@@ -25,3 +29,37 @@ function listLoad() {
 }
 
 listLoad();
+
+let id = document.getElementById('id');
+let name = document.getElementById('name');
+let rubricLoader = new XMLHttpRequest();
+
+rubricLoader.addEventListener('readystatechange', () => {
+    if (rubricLoader.readyState == 4) {
+        if (rubricLoader.status == 200) {
+            let data = JSON.parse(rubricLoader.responseText);
+            id.value = data.id;
+            name.value = data.name;
+        } else
+            window.alert(rubricLoader.statusText);
+    }
+});
+
+function rubricLoad(evt) {
+    evt.preventDefault();
+    rubricLoader.open('GET', evt.target.href, true);
+    rubricLoader.send();
+}
+
+let form = document.getElementById('rubric_form');
+
+form.addEventListener('submit', function(evt) {
+    evt.preventDefault();  // отменяем обычную отправку
+
+    let saveLoader = new XMLHttpRequest();
+    saveLoader.open('POST', domain + '/api/rubrics/', true);
+    saveLoader.setRequestHeader('Content-Type', 'application/json');
+    saveLoader.send(JSON.stringify({
+        name: name.value
+    }));
+});
