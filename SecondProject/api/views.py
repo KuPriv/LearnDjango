05a79283d5fdc63d1990c57fbc3c0894f13698ef
sezen <sezen.egor@mail.ru>
2026-02-27 13:28:34 +0300

    feat: add Rubric API views for CRUD operations

--- SecondProject/api/views.py
+++ SecondProject/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import get_object_or_404
+from django.http import Http404
+
 from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
 
 from .authentication import CsrfExemptSessionAuthentication
 from .serializers import RubricSerializer
@@ -36,5 +40,42 @@ def api_rubric_detail(request, pk):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == "DELETE":
+        rubric.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class APIRubrics(APIView):
+    def get(self, request):
+        rubrics = Rubric.objects.all()
+        serializer = RubricSerializer(rubrics, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = RubricSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class APIRubricDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Rubric, pk=pk)
+
+    def get(self, request, pk):
+        rubric = self.get_object(pk)
+        serializer = RubricSerializer(rubric)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        rubric = self.get_object(pk)
+        serializer = RubricSerializer(rubric, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        rubric = self.get_object(pk)
         rubric.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
