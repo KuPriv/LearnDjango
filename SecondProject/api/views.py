010b4db05c26f1d5dfaa2ee6c3457835509f3fd6
sezen <sezen.egor@mail.ru>
2026-02-27 14:07:37 +0300

    feat: add IsAuthenticatedOrReadOnly permission to APIRubricViewSet

--- SecondProject/api/views.py
+++ SecondProject/api/views.py
@@ -1,14 +1,6 @@
-from django.shortcuts import get_object_or_404
-from django.http import Http404
-
-from rest_framework.decorators import api_view, authentication_classes
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
-from .authentication import CsrfExemptSessionAuthentication
 from .serializers import RubricSerializer
 from app.models import Rubric
 
@@ -16,3 +8,4 @@ from app.models import Rubric
 class APIRubricViewSet(ModelViewSet):
     queryset = Rubric.objects.all()
     serializer_class = RubricSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
